026c3205efd5dc1cb7328635d8fce7b556578e35
Svetlana <spo_17@mail.ru>
2026-03-29 20:28:14 +0800

    Initial Commit

---
+++ TravelSchedule/TravelScheduleApp.swift
@@ -0,0 +1,21 @@
+//
+//  TravelScheduleApp.swift
+//  TravelSchedule
+//
+//  Created by Svetlana on 2026/3/29.
+//
+
+import SwiftUI
+import CoreData
+
+@main
+struct TravelScheduleApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
